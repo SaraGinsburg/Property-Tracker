d00d3e78a8ee5d66e50981411a5daef2cd1705dd
Sara <Sara.Ginsburg.ny@gmail.com>
2025-02-03 16:23:27 -0500

    fetch Property data using a server component

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,9 +1,15 @@
-import properties from '@/properties.json';
 import PropertyCard from './PropertyCard';
 import Link from 'next/link';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
-const HomeProperties = () => {
-  const recentProperties = properties.slice(0, 3);
+const HomeProperties = async () => {
+  await connectDB();
+
+  const recentProperties = await Property.find({})
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
   return (
     <>
       <section className='px-4 py-6'>
@@ -11,15 +17,15 @@ const HomeProperties = () => {
           <h2 className='text-3xl font-bold text-customDarkBlue mb-6 text-center'>
             Newly Available Rentals
           </h2>
-          {properties.length === 0 ? (
-            <p>No properties found</p>
-          ) : (
-            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-              {recentProperties.map((property) => (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {recentProperties.length === 0 ? (
+              <p>No Properties Found</p>
+            ) : (
+              recentProperties.map((property) => (
                 <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
+              ))
+            )}
+          </div>
         </div>
       </section>
       <section className='m-auto max-w-lg my-6 px-6'>
