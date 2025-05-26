a3f8589b3b1c79baddd867ab3ddf34d0c199c948
Sara <Sara.Ginsburg.ny@gmail.com>
2025-05-25 21:26:51 -0400

    serialize lean documents

--- utils/convertToObject.js
+++ utils/convertToObject.js
@@ -4,21 +4,11 @@
  * @param {Object} leanDocument - The Mongoose lean document to be converted.
  * @returns {Object} A plain JavaScript object that is a serializable representation of the input document.
  */
+
 export function convertToSerializeableObject(leanDocument) {
-  const serialized = { ...leanDocument };
-
-  for (const key of Object.keys(serialized)) {
-    const value = serialized[key];
-
-    if (
-      value &&
-      typeof value === 'object' &&
-      typeof value.toJSON === 'function' &&
-      typeof value.toString === 'function'
-    ) {
-      serialized[key] = value.toString();
-    }
+  for (const key of Object.keys(leanDocument)) {
+    if (leanDocument[key].toJSON && leanDocument[key].toString)
+      leanDocument[key] = leanDocument[key].toString();
   }
-
-  return serialized;
+  return leanDocument;
 }
