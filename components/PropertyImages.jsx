fca9f4526573df6f641c5985cf59321fe138bdf9
Sara <Sara.Ginsburg.ny@gmail.com>
2025-07-08 13:37:40 -0400

    photoswipe lightbox

--- components/PropertyImages.jsx
+++ components/PropertyImages.jsx
@@ -1,42 +1,66 @@
+'use client';
 import Image from 'next/image';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 
 const PropertyImages = ({ images }) => {
   return (
-    <section className='bg-customVeryLightBluePlus p-4'>
-      <div className='container mx-auto'>
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt=''
-            className='object-cover h-[400px] mx-auto rounded-xl'
-            width={1800}
-            height={400}
-            priority={true}
-          />
-        ) : (
-          <div className='grid grid-cols-2 gap-4'>
-            {images.map((image, index) => (
-              <div
-                key={index}
-                className={`${
-                  index === 2 && images.length === 3
-                    ? 'col-span-2'
-                    : 'col-span-1'
-                }`}>
+    <Gallery>
+      <section className='bg-customVeryLightBluePlus p-4'>
+        <div className='container mx-auto'>
+          {images.length === 1 ? (
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width='1000'
+              height='600'>
+              {({ ref, open }) => (
                 <Image
-                  src={image}
+                  ref={ref}
+                  onClick={open}
+                  src={images[0]}
                   alt=''
-                  className='object-cover h-[400px] w-full rounded-xl'
+                  className='cursor-pointer object-cover h-[400px] mx-auto rounded-xl'
                   width={1800}
                   height={400}
                   priority={true}
                 />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+              )}
+            </Item>
+          ) : (
+            <div className='grid grid-cols-2 gap-4'>
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className={`${
+                    index === 2 && images.length === 3
+                      ? 'col-span-2'
+                      : 'col-span-1'
+                  }`}>
+                  <Item
+                    original={image}
+                    thumbnail={image}
+                    width='1000'
+                    height='600'>
+                    {({ ref, open }) => (
+                      <Image
+                        ref={ref}
+                        onClick={open}
+                        src={image}
+                        alt=''
+                        className='cursor-pointer object-cover h-[400px] w-full rounded-xl'
+                        width={1800}
+                        height={400}
+                        priority={true}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Gallery>
   );
 };
 
--- utils/convertToObject.js
+++ utils/convertToObject.js
@@ -4,11 +4,17 @@
  * @param {Object} leanDocument - The Mongoose lean document to be converted.
  * @returns {Object} A plain JavaScript object that is a serializable representation of the input document.
  */
-
 export function convertToSerializeableObject(leanDocument) {
   for (const key of Object.keys(leanDocument)) {
-    if (leanDocument[key].toJSON && leanDocument[key].toString)
-      leanDocument[key] = leanDocument[key].toString();
+    const value = leanDocument[key];
+    if (
+      value !== null &&
+      value !== undefined &&
+      value.toJSON &&
+      value.toString
+    ) {
+      leanDocument[key] = value.toString();
+    }
   }
   return leanDocument;
 }
