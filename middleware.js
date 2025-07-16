9cef3b5599afc2a4882b767ad0d62dc395a11b27
Sara <Sara.Ginsburg.ny@gmail.com>
2025-07-16 13:53:22 -0400

    minor changes

--- middleware.js
+++ middleware.js
@@ -1,12 +1,5 @@
 export { default } from 'next-auth/middleware';
 
-// middleware.js
-import { withAuth } from 'next-auth/middleware';
-
-// export default withAuth({
-//   // Optional: add custom logic or callbacks here
-// });
-
 export const config = {
   matcher: ['/properties/add', '/profile', '/properties/saved', '/messages'],
 };
